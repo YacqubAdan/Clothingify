4aa3375e7e7e835287dd481e13f5e3e3fe8fa41d
YacqubAdan <yacquubadan.1@gmail.com>
2022-06-07 20:43:06 +0100

    imported express corse,routes modules and connected to db

--- server/server.js
+++ server/server.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cartRoute = require("./routes/cart");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+
+require("dotenv").config();
 
 //App Config
 const app = express();
 const port = process.env.PORT || 5000;
 
-const users = ["userOne", "userTwo", "userThree"];
+//Connection to MongoDB
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connection successful"))
+  .catch((err) => console.log(err));
 
 //Middlewares
 app.use(cors());
-//DB config
+app.use(express.json());
 
-//API Endpoints
+//API Routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
+app.use("/api/cart", cartRoute);
+
+//defualt route
 app.get("/", (req, res) => {
   res.status(200).send("CLOTHINGIFY!");
 });
 
-app.get("/api", (req, res) => {
-  res.json({ users });
-});
-
+// app listening to port 5000 or process.env.PORT
 app.listen(port, () => {
   console.log("listening in port 5000");
 });
